a7f0f0779ab43c289e6e2b7fb06e66a15e8406aa
Ateeb Ahmed Malik <68679919+Ateebmalik9@users.noreply.github.com>
2023-07-15 16:40:13 +0530

    Update insert, delete (updated) in linked list.c

--- insert, delete (updated) in linked list.c
+++ insert, delete (updated) in linked list.c
@@ -84,6 +84,11 @@ void Insert()
 	int item;
 	trav=head;
 	
+	temp=(struct node*)malloc(sizeof(struct node));
+	printf("Enter node to insert: ");
+	scanf("%d",&(temp->data));
+	temp->next=NULL;
+	
 	if(control==1)
 	{
 		printf("Enter the node after which you want to insert: ");
@@ -91,11 +96,6 @@ void Insert()
 	
 		while(trav->data!=item)
 			trav=trav->next;
-		
-		temp=(struct node*)malloc(sizeof(struct node));
-		printf("Enter node to insert: ");
-		scanf("%d",&(temp->data));
-		temp->next=NULL;
 	
 		if(trav==tail)// data item to stored after tail
 		{
@@ -113,29 +113,17 @@ void Insert()
 		scanf("%d",&item);
 		if(item==head->data)
 		{
-			temp=(struct node*)malloc(sizeof(struct node));
-			printf("Enter node to insert: ");
-			scanf("%d",&(temp->data));
 			temp->next=head;
-			
 			head=temp;
 		}
 		 else{
 		 	while(trav->next->data!=item)	
 				trav=trav->next;
-			
-			temp=(struct node*)malloc(sizeof(struct node));
-			printf("Enter node to insert: ");
-			scanf("%d",&(temp->data));
-			temp->next=NULL;
-		
-
+	
 			temp->next=trav->next;
 			trav->next=temp;
 		 	
 		 }
-		
-		
 	}
 }
 
